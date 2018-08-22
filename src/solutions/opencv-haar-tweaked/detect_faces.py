10ab25b49c10db2d8e56f69124fc1dd577ec2c7e
David Pacassi Torrico <dpacassi@gmail.com>
2018-08-22 16:51:42 +0200

    Try to tweak OpenCV haar

--- src/solutions/opencv-haar-tweaked/detect_faces.py
+++ src/solutions/opencv-haar-tweaked/detect_faces.py
@@ -21,8 +21,7 @@ def main():
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # Run the face detector on the image data.
-    #detected_faces = haar_face_cascade.detectMultiScale(image, scaleFactor=1.1, minNeighbors=10, minSize=(25, 25));
-    detected_faces = haar_face_cascade.detectMultiScale(image);
+    detected_faces = haar_face_cascade.detectMultiScale(image, scaleFactor=1.05, minNeighbors=12);
 
     # Loop through each detected face.
     for (x, y, w, h) in detected_faces:
