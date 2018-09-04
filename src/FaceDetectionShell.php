8366b2b6f2c6d92c9d277aa578025875e22a904c
David Pacassi Torrico <dpacassi@gmail.com>
2018-09-04 12:46:02 +0200

    Add Ageitgrey's face recognition solution

--- src/FaceDetectionShell.php
+++ src/FaceDetectionShell.php
@@ -27,4 +27,27 @@ class FaceDetectionShell {
 
     return json_decode($result, TRUE);
   }
+
+  public function detectFacesFromList(FaceDetectionImage $image) {
+    $faces = [];
+    $result = shell_exec($this->shellCommand . ' ' . $image->getImage());
+    $result = trim($result);
+
+    if (!empty($result)) {
+      $faces_in_image = preg_split('/\r\n|\r|\n/', $result);
+
+      foreach ($faces_in_image as $face_in_image) {
+        $face_in_image = explode(',', $face_in_image);
+
+        $faces[] = [
+          'left' => $face_in_image[4],
+          'top' => $face_in_image[1],
+          'right' => $face_in_image[2],
+          'bottom' => $face_in_image[3],
+        ];
+      }
+    }
+
+    return $faces;
+  }
 }
