c6ba7a9a530ad69a12243714ef336faab68ebed7
David Pacassi Torrico <dpacassi@gmail.com>
2018-09-04 11:21:31 +0200

    Add the OpenCV deep learning solution

---
+++ src/solutions/opencv-deep-learning/detect_faces.py
@@ -0,0 +1,42 @@
+import json
+import sys
+import cv2
+import numpy as np
+
+def main(minmum_confidence):
+    faces_result = []
+
+    # Take the image file name from the command line.
+    file_name = sys.argv[1]
+
+    # Load the image, resize to 300x300 pixels and normalize it.
+    image = cv2.imread(file_name)
+    (h, w) = image.shape[:2]
+    blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
+
+    # Load our serialized model file.
+    net = cv2.dnn.readNetFromCaffe('data/deploy.prototxt.txt', 'data/res10_300x300_ssd_iter_140000.caffemodel')
+
+    # Run the face detector on the image data.
+    net.setInput(blob)
+    detections = net.forward()
+
+    # Loop through each detected face.
+    for i in range(0, detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+
+        if confidence > minmum_confidence:
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype('int')
+            faces_result.append({
+                "left": int(startX),
+                "top": int(startY),
+                "right": int(endX),
+                "bottom": int(endY)
+            })
+
+    print(json.dumps(faces_result))
+
+
+if __name__=="__main__":
+    main(0.5)
--- src/solutions/opencv-haar/detect_faces.py
+++ src/solutions/opencv-haar/detect_faces.py
@@ -21,7 +21,7 @@ def main():
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # Run the face detector on the image data.
-    detected_faces = haar_face_cascade.detectMultiScale(image);
+    detected_faces = haar_face_cascade.detectMultiScale(image)
 
     # Loop through each detected face.
     for (x, y, w, h) in detected_faces:
